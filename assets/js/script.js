1016b733498e24320ccc6e4c9c5e1ff34286b77f
Paulina San Martin <puli.sanmartin@gmail.com>
2024-02-22 16:26:39 -0400

    Traspaso de url desde el html al js;instancias agregadas; funciones privada y pública listas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,64 @@
-const mostrarVideo= (()=>{
-    let video=(function (){
-        document.getElementById('headingOne').addEventListener( playMultimedia:()=>{
-            document.getElementById('musica')
-        })
-        return{
-            playMultimedia:()=>{},
-        }
-    });
-
-})();
-
-
-class Multimedia{
-constructor (url,inicio){
-    this._url= url;
-    this._inicio=inicio;
-}
-get url(){
+//clases//
+class Multimedia {
+  constructor(url) {
+    this._url = url;
+  }
+  get url() {
     return this._url;
-}
-set url(nueva_url){
-    this._url= nueva_url;
-}
-get inicio(){
-    return this._inicio
-}
+  }
+  set url(nueva_url) {
+    this._url = nueva_url;
+  }
 
-setInicio(){
-    return ``
-}
-
+  setInicio() {
+    return `Este método es para realizar un cambio en la URL del video`;
+  }
 }
 
 class Reproductor extends Multimedia {
-constructor(url,inicio,id){
-    super(url,inicio)
-    this._id=id
-}
-setInicio(){
-playMultimedia(){
+  constructor(url, id) {
+    super(url);
+    this._id = id;
+  }
+  get id(){
+    return this._id
+  };
+  set id(nuevo_id){
+    this._id=nuevo_id;
+  }
 
-};
-};
+  setInicio(tiempo) {
+    this.id.setAtribute("src", `${this.url}?start=${tiempo}`);
+  }
+  playMultimedia() {
+    mostrarVideo.videoPublico(this._url,this._id);   //Deja esta parte para después de hacer la función privada//
+  }
 }
+//función privada y funció pública
+let mostrarVideo = (() => {
+  let videoPrivado = (url, id) => {
+    let element = document.getElementById(id);
+    element.setAttribute("src", url);
+
+  };
+  return {
+    videoPublico: (url, id) => {
+      videoPrivado(url, id);
+    },
+  };
+})();
+
+
+///instanciar
+let musica= new Reproductor('https://www.youtube.com/embed/Lv_oZaL5nSI?si=FOpUqvKeQvrgRh1h','musica');
+let peliculas= new Reproductor('https://www.youtube.com/embed/l9tP4M8URhQ?si=bljmBLu0MkgcsnTl','peliculas');
+let series= new Reproductor('https://www.youtube.com/embed/Gb0nQe8eLVw?si=CZcE_fq0co8IUlqX','series')
+
+//metodos que modifican la url
+musica.playMultimedia()
+peliculas.playMultimedia()
+series.playMultimedia()
+
+musica.setInicio(1);
+series.setInicio(2);
+peliculas.setInicio(1)
