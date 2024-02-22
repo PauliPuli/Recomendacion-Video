780da4587e2bc61cb9ae0802bb154e5983b0f85c
Paulina San Martin <puli.sanmartin@gmail.com>
2024-02-22 15:11:07 -0400

    descripción de las clases Multimedia y Reproductoren script.js; incrustar url de iframes en el html

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+const mostrarVideo= (()=>{
+    let video=(function (){
+        document.getElementById('headingOne').addEventListener( playMultimedia:()=>{
+            document.getElementById('musica')
+        })
+        return{
+            playMultimedia:()=>{},
+        }
+    });
+
+})();
+
+
+class Multimedia{
+constructor (url,inicio){
+    this._url= url;
+    this._inicio=inicio;
+}
+get url(){
+    return this._url;
+}
+set url(nueva_url){
+    this._url= nueva_url;
+}
+get inicio(){
+    return this._inicio
+}
+
+setInicio(){
+    return ``
+}
+
+}
+
+class Reproductor extends Multimedia {
+constructor(url,inicio,id){
+    super(url,inicio)
+    this._id=id
+}
+setInicio(){
+playMultimedia(){
+
+};
+};
+}
